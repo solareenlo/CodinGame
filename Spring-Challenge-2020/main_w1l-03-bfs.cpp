18e7e15a9ccb76043c5467723f8d91efd91fffa8
solareenlo <solareenlo@gmail.com>
2020-05-15 12:59:23 +0900

    from を両端に寄せたときの最短経路取得

--- Spring-Challenge-2020/main_w1l-03-bfs.cpp
+++ Spring-Challenge-2020/main_w1l-03-bfs.cpp
@@ -25,10 +25,67 @@ constexpr double INF_INT = numeric_limits<int>::infinity();
 constexpr int DX[4] = {1, 0, -1, 0};
 constexpr int DY[4] = {0, 1, 0, -1};
 
-inline void mapTranslation(VVI &from, VVI &to, P ori) {
+inline VVI calDist(P from, P to, VVI map) {
+    int height = map.size();
+    int width = map[0].size();
+
+    /* 幅優先探索の初期設定 */
+    VVI dist(height, VI(width, -1)); // 各セルの最短距離 (訪れていないところは -1 としておく)
+    dist[from.second][from.first] = 0; // スタートを 0 に設定
+    QP que; // 「一度見た頂点」のうち「まだ訪れていない頂点」を表すキュー
+    que.push(make_pair(from.first, from.second)); // スタートを push
+
+    /* キューが空になるまで */
+    while (!que.empty()) {
+        P currentPos = que.front(); // キューの先頭を見る
+        int x = currentPos.first;
+        int y = currentPos.second;
+        que.pop(); // キューから先頭を pop する
+
+        // 隣接頂点を探索
+        REP(direction, 4) {
+            int nextX = x + DX[direction];
+            int nextY = y + DY[direction];
+            if (nextX < 0 || nextX >= width || nextY < 0 || nextY >= height)
+                continue; // 場外アウトはだめ
+            if (map[nextY][nextX] == 0)
+                continue; // 壁はだめ
+            // まだ見ていない頂点なら push
+            if (dist[nextY][nextX] == -1) {
+                que.push(make_pair(nextX, nextY));
+                dist[nextY][nextX] = dist[y][x] + 1; // next の距離も更新
+            }
+        }
+    }
+    return dist;
+}
+
+inline void mapTranslation(VVI &from, VVI &to, P ori, char c) {
     int height = from.size();
     int width = from[0].size();
-    int diffX =  ori.first - (width / 2);
+    int diffX = 0;
+    if (c == 'R') { // ori を右端に持っていく
+        for (int j = ori.first + 1; j < width; j++)
+            REP(i, height)
+                to.at(i).at(j - (ori.first + 1)) = from.at(i).at(j);
+        for (int j = 0; j < ori.first + 1; j++)
+            REP(i, height)
+                to.at(i).at(j + (width - ori.first - 1)) = from.at(i).at(j);
+    }
+    else if (c == 'L') { // ori を左端に持っていく
+        for (int j = ori.first; j < width; j++)
+            REP(i, height)
+                to.at(i).at(j - ori.first) = from.at(i).at(j);
+        for (int j = 0; j < ori.first; j++)
+            REP(i, height)
+                to.at(i).at(j + (width - ori.first)) = from.at(i).at(j);
+    }
+}
+
+inline void mapTranslation2Center(VVI &from, VVI &to, P ori) {
+    int height = from.size();
+    int width = from[0].size();
+    int diffX = diffX =  ori.first - (width / 2); // ori を中心に持っていく
     if (diffX <= 0) {
         for (int j = ori.first; j < width + diffX; j++)
             REP(i, height)
@@ -52,80 +109,36 @@ inline void mapTranslation(VVI &from, VVI &to, P ori) {
     }
 }
 
-inline int calDist(P from, P to, VVI map) {
+inline int calShortestDist(P from, P to, VVI map) {
     int height = map.size();
     int width = map[0].size();
     int diffX =  from.first - (width / 2);
-    VVI field(height, VI(width, 0));
-    // cerr << "map: " << height << ' ' << width << endl;
-    // cerr << "ori: " << from.second << ' ' << from.first << endl;
-    // REP(i, height) {
-    //     REP(j, width) cerr << setw(2) << map[i][j] << ' ';
-    //     cerr << endl;
-    // }
+    VVI fieldCenter(height, VI(width, 0)); // from が中央に持っていく
+    VVI fieldLeft(height, VI(width, 0)); // from が左端に持っていく
+    VVI fieldRight(height, VI(width, 0)); // from を右端に持っていく
+    int x = 0, y = 0;
 
     bool loop = false;
     REP(i, height)
         if (map[i][0] == 1)
             loop = true;
-    cerr << "mapTranslation: " << loop << endl;
     if (loop) {
-        mapTranslation(map, field, from);
-        from.first -= diffX;
+        mapTranslation(map, fieldRight, from, 'R');
+        P fromRight = make_pair(width - 1, from.second);
+        VVI distRight = calDist(fromRight, to, fieldRight);
+
+        mapTranslation(map, fieldLeft, from, 'L');
+        P fromLeft = make_pair(0, from.second);
+        VVI distLeft = calDist(fromLeft, to, fieldLeft);
+
+        mapTranslation2Center(map, fieldCenter, from);
+        P fromCenter = make_pair(from.first - diffX, from.second);
+        VVI distCenter = calDist(fromCenter, to, fieldCenter);
+        VVI tmp(height, VI(width, -1));
+        P inverse = make_pair(width - 1 - from.first, y);
+        mapTranslation2Center(distCenter, tmp, inverse);
     } else {
-        REP(i, height) REP(j, width)
-            field.at(i).at(j) = map.at(i).at(j);
-    }
-
-    // REP(i, height) {
-    //     REP(j, width) cerr << setw(2) << field[i][j] << ' ';
-    //     cerr << endl;
-    // }
-    cerr << "ori: " << from.second << ' ' << from.first << endl;
-
-    /* 幅優先探索の初期設定 */
-    VVI dist(height, VI(width, -1)); // 各セルの最短距離 (訪れていないところは -1 としておく)
-    dist[from.second][from.first] = 0; // スタートを 0 に設定
-    QP que; // 「一度見た頂点」のうち「まだ訪れていない頂点」を表すキュー
-    que.push(make_pair(from.first, from.second)); // スタートを push
-
-    /* キューが空になるまで */
-    while (!que.empty()) {
-        P currentPos = que.front(); // キューの先頭を見る
-        int x = currentPos.first;
-        int y = currentPos.second;
-        // cerr << x << ' ' << y << endl;
-        que.pop(); // キューから先頭を pop する
-
-        // 隣接頂点を探索
-        REP(direction, 4) {
-            int nextX = x + DX[direction];
-            int nextY = y + DY[direction];
-            if (nextX < 0 || nextX >= width || nextY < 0 || nextY >= height)
-                continue; // 場外アウトはだめ
-            if (field[nextY][nextX] == 0)
-                continue; // 壁はだめ
-            // まだ見ていない頂点なら push
-            if (dist[nextY][nextX] == -1) {
-                que.push(make_pair(nextX, nextY));
-                dist[nextY][nextX] = dist[y][x] + 1; // next の距離も更新
-            }
-        }
-    }
-
-    cerr << width << ' ' << height << endl;
-    REP(i, height) {
-        REP(j, width) cerr << setw(2) << dist[i][j] << ' ';
-        cerr << endl;
-    }
-
-    cerr << width << ' ' << height << endl;
-    VVI tmp(height, VI(width, -1));
-    P inverse = make_pair(width - 1 - (from.first + diffX), from.second);
-    mapTranslation(dist, tmp, inverse);
-    REP(i, height) {
-        REP(j, width) cerr << setw(2) << tmp[i][j] << ' ';
-        cerr << endl;
+        VVI sidt = calDist(from, to, map);
     }
     return 0;
 }
@@ -202,7 +215,7 @@ int main() {
         // for (auto itr = pellet10.begin(); itr != pellet10.end(); itr++) {
         //     cerr << itr->first << ' ' << itr->second << endl;
         // }
-        int tmp = calDist(myPac[0], pellet.begin()->first, map);
+        int tmp = calShortestDist(myPac[0], pellet.begin()->first, map);
         end = chrono::system_clock::now();
 
         // Write an action using cout. DON'T FORGET THE "<< endl"
